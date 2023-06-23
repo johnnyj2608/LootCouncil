bfbfbcb56c3fe7b8e44dfcfe938497f61ed0e356
Johnny Jiang <johnnyjiang@Johnnys-MBP.domain>
2023-06-23 04:12:40 -0400

    Removed received response.get

--- main.py
+++ main.py
@@ -8,19 +8,22 @@ from wcl import get_perf
 
 def main():
     sheet = '1TyYdcyq2_J5GT6rsIH9mNQgKWtoOa7bxDriMf8u1d5Q'
-    
+
+    # Physical spreadsheet prio
     physical_id = '594385335'
     url = f'https://docs.google.com/spreadsheets/d/{sheet}/export?gid={physical_id}&format=csv'
     df = pd.read_csv(url, skiprows=[0])
     df = df[['Item - Horde', 'Prio']]
     items = df.set_index('Item - Horde').to_dict()['Prio']
 
+    # Caster spreadsheet prio
     caster_id = '1620926651'
     url = f'https://docs.google.com/spreadsheets/d/{sheet}/export?gid={caster_id}&format=csv'
     df = pd.read_csv(url, skiprows=[0])
     df = df[['Item - Horde', 'Prio']]
     items.update(df.set_index('Item - Horde').to_dict()['Prio'])
 
+    # Ulduar specific prio
     items['Pharos Gloves'] = 'Fire > Lock > Boomy/Spriest > Ele'
     items['Flare of the Heavens'] = 'Demo > Fire/Aff > Boom/Spriest/Ele'
     items['Conductive Seal'] = 'Demo > Caster DPS > Healers'
@@ -33,31 +36,25 @@ def main():
     items['Armbands of Bedlam'] = 'UH Dk'
     items['Frigid Strength of Hodir'] = 'Frost Dk'
     items['Crown of Luminescence'] = 'Priest'
-   
 
-    wishlist = 'https://thatsmybis.com/15596/raid-team-two/export/loot/html/wishlist'
-    received = 'https://thatsmybis.com/15596/raid-team-two/export/loot/html/received'
 
-    # Get raider name, class, and sort order
-    response = curl(wishlist)
+    # Create dict to store character info
+    tmb = 'https://thatsmybis.com/15596/raid-team-two/export/loot/html/all'
+    response = curl(tmb)
     csvStringIO = StringIO(response.text)
     df = pd.read_csv(csvStringIO, sep=",")
-    df = df[df['received_at'].isnull()]
+    wl = prio = df[df['type']=='wishlist']
+    rd = df[df['type']=='received']
 
-    prio = {k: f.groupby('character_name')['sort_order'].apply(list).to_dict()
-     for k, f in df.groupby('item_name')}
+    # Gets name and class of character
+    wl = wl[['character_name', 'character_class']].drop_duplicates()
+    wl = wl.rename(columns={'character_class': 'Class'})
+    raiders = wl.set_index('character_name').T.to_dict()
 
-    df = df[['character_name', 'character_class']].drop_duplicates()
-    df = df.rename(columns={'character_class': 'Class'})
-    raiders = df.set_index('character_name').T.to_dict()
-
-    # Get raider received count (Unnecessary? Just count entries if NaN?)
-    response = curl(received)
-    csvStringIO = StringIO(response.text)
-    df = pd.read_csv(csvStringIO, sep=",")
-    df = df[['character_name']]
-    df = df.groupby(df.columns.tolist(), as_index=False).size()
-    temp = df.set_index('character_name').to_dict()['size']
+    # Gets received count of character
+    rd = rd[['character_name']]
+    rd = rd.groupby(rd.columns.tolist(), as_index=False).size()
+    temp = rd.set_index('character_name').to_dict()['size']
     for key in raiders:
         try:
             raiders[key]['Received'] = temp[key]
@@ -66,11 +63,17 @@ def main():
 
     # Intersect today's raiders and get performance
     report_names = get_names(code='bPpcTmQrzGXdMxA6')
-    raiders = {k: raiders[k] for k in report_names if k in raiders}
+    raiders = {k: raiders[k] for k in raiders.keys() & report_names}
     for key in raiders:
         raiders[key].update(get_perf(name=key))
 
-    # filter wishlist if didn't show up for raid
+    # Need to ensure akka's isn't appended. Why is it float now?
+    prio = {k: f.groupby('character_name')['sort_order'].apply(list).to_dict()
+     for k, f in prio.groupby('item_name')}
+    
+    #pp.pprint(raiders)
+    #pp.pprint(prio)
+    #return
 
     for key in prio:
         score = float('inf')
@@ -78,16 +81,16 @@ def main():
         for name in prio[key]:
             cur = prio[key][name][0]
             cur += raiders[name]['Received']
-            cur += wcl_perf(raiders[name]['Best'])
-            cur += wcl_perf(raiders[name]['Med'])
+            cur += perf_score(raiders[name]['Best'])
+            cur += perf_score(raiders[name]['Med'])
             # parse items prio here
             if cur < score:
                 score = cur
                 res = name
         prio[key] = res
-    print(prio)
+    pp.pprint(prio)
 
-def wcl_perf(perf):
+def perf_score(perf):
     if perf > 95:
         return 1
     if perf > 75:
